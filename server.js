6a7ed20d032c903d47d7332f6519a387b91f4f33
Bhavani Sathi <bhavanisathi08@gmail.com>
2021-04-29 09:52:53 +0530

    f commit

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", function(req, res){
     res.send("Welcome to Sweety's page");
 })
+app.get("/home", function(req, res){
+    res.send("Welcome to home page");
+})
  
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
