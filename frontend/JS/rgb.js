aa9ddecb44befc4e69fae146248a1a7595260786
Bhavani Sathi <bhavanisathi08@gmail.com>
2021-05-06 17:47:16 +0530

    finalrgb

--- frontend/JS/rgb.js
+++ frontend/JS/rgb.js
@@ -2,21 +2,21 @@ var re=0,gr=0,bl=0;
 function rshow(rs)
 {
     re=rs;
-    alert(re+""+gr+""+bl);
     var bg="rgb("+re+","+gr+","+bl+")";
    document.getElementById("red").style.backgroundColor=bg;
+   document.getElementById("reddis").innerHTML=rs;
 }
 function gshow(gs)
 {
     gr=gs;
-    alert(re+""+gr+""+bl);
     var bg="rgb("+re+","+gr+","+bl+")";
    document.getElementById("red").style.backgroundColor=bg;
+   document.getElementById("grdis").innerHTML=gs;
 }
 function bshow(bs)
 {
     bl=bs;
-    alert(re+""+gr+""+bl);
     var bg="rgb("+re+","+gr+","+bl+")";
    document.getElementById("red").style.backgroundColor=bg;
+   document.getElementById("bldis").innerHTML=bs;
 }
